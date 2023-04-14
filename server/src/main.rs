91729f7df9ebd97ab3a2c1d7ba8e22cae3faa951
Luna Borella <ulfrthewolf@gmail.com>
2023-04-14 21:05:57 +1000

    Made a little static file server.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,2 +1,15 @@
-fn main() {
+use axum::Router;
+use tower_http::services::{ServeDir, ServeFile};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = Router::new()
+        .nest_service("/", ServeDir::new("../site").not_found_service(ServeFile::new("../site/index.html")))
+        .nest_service("/crate", ServeDir::new("../crate"));
+
+    axum::Server::bind(&"0.0.0.0:2611".parse()?)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
 }
